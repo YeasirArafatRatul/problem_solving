4f38ecf5d8a189ee7dd19b8a3886be2ac2ab64c6
arafat_ratul <yeasirarafat.ratul@gmail.com>
2019-11-03 23:36:27 +0600

    1025

--- timus/t1025.py
+++ timus/t1025.py
@@ -1,6 +1,9 @@
-from math import ceil
 k = int(input())
 
 supporters = list(map(int,input().split(' ')))
+supporters.sort()
+total = 0
+for x in range(k//2+1):
+    total += supporters[x]//2+1
 
-print(ceil(sum(supporters)/k))
+print(total)
