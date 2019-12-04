36aded349da3cf5945c9d2e7bfdd7572331000d1
arafat_ratul <yeasirarafat.ratul@gmail.com>
2019-12-04 23:33:13 +0600

    brents loop detection updated

--- uVA/pseudo random numbers.py
+++ uVA/pseudo random numbers.py
@@ -5,38 +5,28 @@ brent's cycle detection algorithm
 '''
 
 def loop_detection(func,seed):
-    power = lam = 1
+    power = count = 1
     tortoise = seed #first_node
     hare = func(seed) #calling the function to give the next value
 
     while tortoise != hare:
-        if power == lam:
-            tortoise = hare
+        if power == count:
+            tortoise = hare #now tortoise has forward to hare's position
             power *= 2
-            lam = 0
-        hare = func(hare)
-        lam +=1
-
-
-    tortoise = hare = seed
-    for i in range(lam):
-        hare = func(hare)
-    while tortoise != hare:
-        tortoise = func(tortoise)
-        hare = func(hare)
-      
-    return lam
-
+            count = 0
+        hare = func(hare) #hare is forwarding to the next random number 
+        count +=1
+        #print(f'count : {count}, power: {power}')
+    return count
 
 
 case = 1    
 while True:
     Z,I,M,L = map(int, input().split())
-    if Z== I== M == L == 0:
+    if Z == I== M == L == 0:
         break
     else:
         def next_value_counter(L):
             return (Z*L+I)%M
         print(f"Case {case}: {loop_detection(next_value_counter,L)}")
         case += 1
-        
